f4ad9cb53f2da877a22b9c943ea176e5025139f1
vksssdks <2820142.cse@pietgroup.co.in>
2024-06-08 15:03:33 +0530

    fixing the platfrom to be used on linux/arm64

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,13 +1,14 @@
 package jwt
 
 import (
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-// var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
-var jwtSecret = []byte("secret")
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+// var jwtSecret = []byte("secret")
 
 func GenerateJWT(username, email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
